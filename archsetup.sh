56f8ed9c4c4fee391683ed0bc01ecf247671f04c
max <>
2025-12-12 11:08:49 +0100

    go back to oh-my-zsh

--- archsetup.sh
+++ archsetup.sh
@@ -2,17 +2,27 @@
 
 cd ~
 
-pacman -Syu
-pacman -S zsh neovim git lazygit curl luarocks gcc ripgrep xclip tree-sitter-cli tmux ghostscript tectonic mermaid-cli fd helm terraform ansible k9s kubectl python3 go fastfetch btop
+mv .bashrc .bashrc.bak
 
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ~
+sudo pacman -Syu
+sudo pacman -S zsh neovim git lazygit curl luarocks gcc ripgrep xclip tree-sitter-cli tmux ghostscript tectonic mermaid-cli fd helm terraform ansible k9s kubectl python3 go fastfetch btop
 
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd ~
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#sudo pacman -S --needed git base-devel
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+#cd ~
+
+#yay -S oh-my-posh
+
+#sudo pacman -S --needed base-devel
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si
+#cd ~
